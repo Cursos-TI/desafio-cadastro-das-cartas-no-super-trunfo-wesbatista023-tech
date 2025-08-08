637678ac9cc916c8ee177aeb98a6fae5313dc064
wesbatista023-tech <wes.batista023@gmail.com>
2025-08-08 01:00:58 +0000

    Carta Super Trunfo Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+char Estado[02];
+char Codigo[04];
+char NomeCidade[50];
+int Populacao;
+float Area;
+float PIB;
+char PontosTuristicos;
+
+printf("Digite aqui o estado: ");
+scanf("%s", &Estado);
+
+printf("Digite aqui o codigo: ");
+scanf("%s", &Codigo);
+
+printf("Digite o nome da cidade: ");
+scanf("%s", &NomeCidade);
+
+printf("Digite a populacao: ");
+scanf("%d", &Populacao);
+
+printf("Digite a area: ");
+scanf("%f", &Area);
+
+printf("Digite aqui o PIB: ");
+scanf("%f", &PIB);
+
+printf("Digite o numero de pontos turisticos: ");
+scanf("%d", &PontosTuristicos);
+
+    printf("\n Carta Super Trunfo\n"); // Capa da carta, teste com \n na frente
+    printf("Estado: %s\n", Estado);
+    printf("Codigo: %s\n", Codigo);
+    printf("Nome da Cidade: %s\n", NomeCidade); // Nome só vai junto.
+    printf("População: %d\n", Populacao);
+    printf("Area: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões de reais\n", PIB);
+    printf("Pontos turisticos: %d\n", PontosTuristicos); // Já refiz várias vezes e não aparece. MDS
+
+    
     return 0;
+
 }
